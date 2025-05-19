d356eeb8b1395d5735a606a7fa65231225674614
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-05-19 13:27:15 +0800

    feat(player): Improve music player UI and song tracking
    - Added `setPlayerDisplay` function for better song information visibility
    - Implemented `highlightCurrentSong` for improved playlist navigation
    - Optimized `sortSongs` function for better song organization

--- #07 - MUSIC PLAYER/script.js
+++ #07 - MUSIC PLAYER/script.js
@@ -98,6 +98,8 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
+  highlightCurrentSong();
+
   audio.play();
 };
 
@@ -127,6 +129,29 @@ const playPreviousSong = () => {
 
     playSong(previousSong.id);
   }
+};
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {
@@ -185,3 +210,4 @@ const sortSongs = () => {
 };
 
 renderSongs(sortSongs());
+
