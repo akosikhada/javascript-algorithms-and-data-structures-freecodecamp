52b4672ef6f125832587f97bd36025071304ebcf
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-06-03 12:19:19 +0800

    feat(course): Implement setPlayerCards function for dynamic player rendering
    - Added  function to dynamically generate player cards
    - Utilized object destructuring for cleaner data extraction
    - Applied conditional rendering for captain designation

--- #09 - FOOTBALL TEAM CARDS/script.js
+++ #09 - FOOTBALL TEAM CARDS/script.js
@@ -179,3 +179,18 @@ typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+      return `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: </p>
+        </div>
+      `;
+    }
+  );
+};
