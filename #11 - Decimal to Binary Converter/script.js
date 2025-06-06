74b7dc660ad3b11d9e52c28d4215753bc304c1b6
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-06-06 22:56:57 +0800

    feat(course): Progress Decimal to Binary Converter (Steps 1-50) with recursion
    - Implemented recursive function calls for structured binary conversion
    - Added input validation to ensure proper decimal number handling
    - Optimized event listeners for user interaction
    - Task in progress—Step 51 and beyond to be completed

---
+++ #11 - Decimal to Binary Converter/script.js
@@ -0,0 +1,60 @@
+const callStack = [
+  'a(): returns "freeCodeCamp " + b()',
+  'b(): returns "is " + c()',
+  'c(): returns "awesome!"',
+];
+
+const a = () => {
+  return "freeCodeCamp " + b();
+};
+
+const b = () => {
+  return "is " + c();
+};
+
+const c = () => {
+  return "awesome!";
+};
+
+console.log(a());
+
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
