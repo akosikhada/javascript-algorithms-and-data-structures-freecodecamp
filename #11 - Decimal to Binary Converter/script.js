fc203ed224731f6437da1cc45050bcbe206f20ce
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-06-06 23:03:21 +0800

    docs: Add TODO comment for call stack management in Decimal to Binary Converter
    - Documented call stack order and execution flow for functions a(), b(), and c()
    - Instructed removal of "c(): returns 'awesome!'" from the callStack array after execution

--- #11 - Decimal to Binary Converter/script.js
+++ #11 - Decimal to Binary Converter/script.js
@@ -4,6 +4,19 @@ const callStack = [
   'c(): returns "awesome!"',
 ];
 
+// TODO: Manage Call Stack
+// 1. Understand the call stack order
+//    a() is at the bottom (first call)
+//    b() is in the middle (called by a())
+//    c() is at the top (called by b())
+// 2. Execution order
+//    Functions execute from the top of the stack downwards (c(), then b(), then a())
+// 3. When c() executes
+//    It returns the string "awesome!"
+//    c() is then popped off (removed) from the top of the callStack array.
+// Action:
+// Remove "c(): returns 'awesome!'" from the top of the callStack array.
+
 const a = () => {
   return "freeCodeCamp " + b();
 };
